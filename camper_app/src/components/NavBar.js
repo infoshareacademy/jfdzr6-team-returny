f221a99c2cc0b63e5d1782e88e7cbc8c3dbfbe5e
Daria Kolesnik <dariakol91@gmail.com>
2022-06-05 20:10:52 +0200

    navbar created

--- camper_app/src/components/NavBar.js
+++ camper_app/src/components/NavBar.js
@@ -1,43 +1,110 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import logo from "../assets/logo.jpg";
+
 
 const StyledNavigation = styled.nav`
-  background-color: yellowgreen;
+  background-color: #373737;
   height: 100px;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+
 `;
 
 const StyledLinkContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
   padding: 20px;
+  margin:20px;
+
 `;
+
+const ButtonGroup = styled.div`
+display:flex;
+justify-content: space-between;
+
+`;
+
+const Button = styled.button`
+background-color:aqua;
+align-items: right;
+padding: 15px 15px;
+background-color:rgb(93, 228, 217);
+border: none;
+border-radius: 20px;
+margin-left:20px;
+
+`;
+
+
+const StyledImgLogo = styled.img`
+
+align-items: left;
+
+`;
+
+
 
 export function NavBar() {
   return (
     <>
       <StyledNavigation>
+
+      <StyledImgLogo src={logo} />
+      
+        
         <StyledLinkContainer>
+
+          
           <NavLink
             className={({ isActive }) =>
               isActive ? "linkcolor linkformat" : "linkformat"
             }
             to="/"
           >
-            Home
+            O NAS
           </NavLink>
+          
           <NavLink
             className={({ isActive }) =>
               isActive ? "linkcolor linkformat" : "linkformat"
             }
-            to="/about"
+            to="/"
           >
-            About
+            ZNAJDŹ CAMPERA
           </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "linkcolor linkformat" : "linkformat"
+            }
+            to="/"
+          >
+            KONTAKT
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "linkcolor linkformat" : "linkformat"
+            }
+            to="/"
+          >
+            UBEZPIECZENIA
+          </NavLink>
+
+          <ButtonGroup>
+            <Button> Zaloguj się </Button>
+            <Button> Dodaj campera </Button>
+          </ButtonGroup>
+
+    
         </StyledLinkContainer>
       </StyledNavigation>
+
+      
+
     </>
   );
 }
