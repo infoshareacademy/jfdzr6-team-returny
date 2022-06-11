fe343ce5585be12dfedcb975348868417a514c3b
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-11 16:57:35 +0200

    modify campercard

--- camper_app/src/components/CamperCard.js
+++ camper_app/src/components/CamperCard.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./CamperCard.css"
+import { useState, useEffect } from "react";
+import {
+  getDocs,
+  deleteDoc,
+  onSnapshot,
+  query,
+  orderBy,
+  collection,
+  doc,
+  serverTimestamp,
+  where,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { getAllCampers,getAllCampersSnapshot } from "../api/getAllCampers";
+const campersCollection = collection(db, "campers");
+
+  
 
 
-const CamperCard = () => {
+export const CamperCard = () => {
+    const [campers, setCampers] = useState([]);
+
+    useEffect(() => {
+        // getAllCampers().then(data=>console.log(data)).catch(err=>console.log(err));
+    
+        onSnapshot(
+          query(campersCollection, orderBy("createdAt", "desc")),
+          (querySnapshot) => {
+            const allCampers = getAllCampersSnapshot(querySnapshot);
+            setCampers(allCampers);
+          }
+        );
+      }, []);
+
+      console.log(campers)
         return(
 
        
@@ -99,4 +133,3 @@ function FindCmpr(){
     )
 }
 
-export default CamperCard;
