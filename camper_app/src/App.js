6c311fb1730a772e386d333840f609639851f14f
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-12 19:36:48 +0200

    add Campers component

--- camper_app/src/App.js
+++ camper_app/src/App.js
@@ -1,11 +1,12 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { AddCamperForm} from "./views/AddCamperForm/AddCamperForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AddCamperForm } from "./views/AddCamperForm/AddCamperForm";
 import { NavBar } from "./components/NavBar";
 import { Home } from "./views/Home";
 import { Footer } from "./components/Footer";
 import { CamperCard } from "./components/CamperCard";
 import { NotificationContainer } from "react-notifications";
 import { PreviewCamp } from "./views/PreviewSingleCamp/PreviewCamp";
+import { Campers } from "./views/Campers";
 
 export function App() {
   return (
@@ -13,27 +14,24 @@ export function App() {
       <BrowserRouter>
         <NavBar />
         <NotificationContainer />
-      
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<Home/>}/>
-          
-          <Route path="contact" element={<Home/>}/>
-          <Route path="insurance" element={<Home/>}/>
-          <Route path="login" element={<Home/>}/>
-          <Route path="add-camper" element={<AddCamperForm />}/>
 
-          <Route path="find-camper" element={<Campers/>}>
-          <Route index element={<CamperCard/>}/>
-          <Route path=":id" element={<PreviewCamp />}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+
+          <Route path="contact" element={<Home />} />
+          <Route path="insurance" element={<Home />} />
+          <Route path="login" element={<Home />} />
+          <Route path="add-camper" element={<AddCamperForm />} />
+
+          <Route path="find-camper" element={<Campers />}>
+            <Route index element={<CamperCard />} />
+            <Route path=":id" element={<PreviewCamp />} />
           </Route>
         </Routes>
 
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-      
-      
     </>
   );
 }
-
