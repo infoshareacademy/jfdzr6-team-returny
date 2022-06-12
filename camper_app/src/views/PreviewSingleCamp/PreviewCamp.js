c40ff729c408333c5cd14d56a44b712e35071ef1
Wojtek Bardyga <wojtek.bardyga@gmail.com>
2022-06-12 19:05:33 +0200

    Add single page view

---
+++ camper_app/src/views/PreviewSingleCamp/PreviewCamp.js
@@ -0,0 +1,40 @@
+import { StyledDescriptionBox, StyledDescription, Wrapper, StyledCampImg, CampTitle, StyledContactDetails, StyledContactHead, StyledCampDetails } from './PreviewCamp.style'
+
+
+
+export function PreviewCamp () {
+    return (
+        <Wrapper>
+
+                <CampTitle>
+                    <h2>Chausson - 627GA Premium</h2>
+                </CampTitle>
+
+                <StyledCampImg>
+                    <img src= "#" alt="Tutaj jest camper"/>
+                </StyledCampImg>
+
+                <StyledCampDetails>
+                    <p>Kategoria</p>
+                    <p>Rocznik</p>
+                    <p>Marka</p>
+                    <p>Ilość osób</p>
+                    <p>Cena (zł/dzień)</p>
+                    <p>Lokalizacja</p>
+                </StyledCampDetails>
+
+                <StyledDescriptionBox>
+                    <StyledDescription>Duża lodówka 134 L z systemem AES 2 oddzielne łóżka wzdłużne 90 cm z możliwością połączenia w łoże małżeńskie 
+                        Przestronny luk bagażowy z dostępem z 2 stron i regulacją wysokości zamykany łazienka z obrotową przegrodą 5 miejsc do jazdy</StyledDescription>
+                </StyledDescriptionBox>
+
+                <StyledContactDetails>
+                    <StyledContactHead>Dane kontaktowe:</StyledContactHead>
+                    <p>Imię:</p>
+                    <p>Telefon:</p>
+                    <p>E-Mail:</p>
+                </StyledContactDetails>
+
+        </Wrapper>
+    )
+}
---
+++ camper_app/src/views/PreviewSingleCamp/PreviewCamp.style.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  font-family: 'Play', sans-serif;
+  margin: 20px auto;
+  width: 50%;
+  padding: 30px;
+  box-shadow: 0 2px 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(210,210,210);
+`;
+
+export const CampTitle = styled.div`
+    font-size: 30px;
+    margin: 20px auto;
+    box-shadow: 0 2px 5px;
+    background-color: white;
+    padding: 10px 10px;
+`;
+
+export const StyledCampImg = styled.div`
+    border: 5px solid #78cdca;
+    margin: auto 0;
+`;
+
+export const StyledDescriptionBox = styled.div`
+    margin: 20px auto;
+    background-color: white;
+`;
+
+export const StyledDescription = styled.p`
+  font-size: 15px;
+  margin: auto 0;
+  box-shadow: 0 2px 5px;
+  padding: 20px;
+`;
+
+export const StyledCampDetails = styled.div`
+    width: 300px;
+    margin: 15px auto;
+    box-shadow: 0 2px 5px;
+    background-color: white;
+    font-size: 15px;
+    padding: 10px;
+`;
+
+export const StyledContactDetails = styled.div`
+    width: 300px;
+    margin: 15px auto;
+    box-shadow: 0 2px 5px;
+    background-color: white;
+    font-size: 15px;
+    padding: 10px;
+`;
+
+export const StyledContactHead = styled.p`
+    font-size: 15px;
+`;
