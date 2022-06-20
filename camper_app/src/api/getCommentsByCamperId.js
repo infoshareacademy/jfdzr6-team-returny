30fd7040d6e411b1e9949b69645545f0b2a7d71c
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-20 13:17:39 +0200

    display comments and basic form

---
+++ camper_app/src/api/getCommentsByCamperId.js
@@ -0,0 +1,17 @@
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export function getCommentsByCapmerId(id) {
+    const docRef = doc(db, "comments", id);
+
+    return getDoc(docRef)
+      .then((querySnapshot) => {
+        return {
+          id: querySnapshot.id,
+          ...querySnapshot.data(),
+        };
+      })
+      .then((data) => {
+        return data;
+      });
+  }
--- camper_app/src/components/UsersComments.js
+++ camper_app/src/components/UsersComments.js
@@ -1,47 +1,86 @@
-import { useEffect, useState } from "react";
-export function UsersComments({camperData}){
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../context/userContext";
+import { getCommentsByCapmerId } from "../api/getCommentsByCamperId";
+import { connectStorageEmulator } from "firebase/storage";
+export function UsersComments({ camperData }) {
+  const [isVisibleAddCommentForm, setisVisibleAddCommentForm] = useState(true);
+  const [comments, setComments] = useState();
+  
+  const context = useContext(UserContext);
+  // if (camperData.campercomments.comments[0]){
+  //     setComments(camperData.campercomments.comments);
+  // }
+  console.log("id campera:", camperData.id);
+  useEffect(() => {
+    getCommentsByCapmerId("d3IY7bPvuyt38xZQ5JN3").then((allcomments) => {
+       setComments(allcomments);
+     
+    });
+  }, []);
 
-
-    return (
-        <>
-        <div style={{border:"black 1px solid"}}>
-        <h3>Komentarze uzytkowników :</h3>
-          {/* {post.comment[0] && post.comment.map((comment,index)=><div key={index}><p>{index+1}: {comment.comment}</p></div> )}
-
-          <button onClick={()=> setisVisibleForm(false)}>Add comment</button>
-          <CommentForm isVisibleForm={isVisibleForm} onCreateComment={onCreateComment} handleCloseForm={handleCloseForm}/> */}
-          </div>
-        </>
-    )
+  console.log(context.userData.name);
+  console.log(comments);
+const handleSubmitComment=(e)=>{
+    e.preventDefault();
+const newcomment= e.target.contentcom.value;
+console.log(newcomment);
 }
 
-export function CommentForm({ isVisibleForm ,onCreateComment, handleCloseForm}) {
-    const [comment, setComment] = useState();
-    // const navigate = useNavigate();
-   
-  
-    // const handleChangeComment = (e) => {
-    //   setComment(e.target.value);
-    // };
-  
-    // const handleSubmitForm = (event) => {
-    //   console.log(comment);
-    //   event.preventDefault();
-    //   onCreateComment({ comment: comment });
-    //   setComment("");
-    //   navigate('/blog');
-  
-    // };
-    return (
-      <>
-        {/* <form onSubmit={handleSubmitForm} hidden={isVisibleForm}>
+
+
+  return (
+    <>
+      <div style={{ border: "black 1px solid" }}>
+        <h3>Komentarze uzytkowników :</h3>
+        {/* {comments && comments.map((comment,index)=><p key={index}>nr {index+1}: {comment}</p> )} */}
+        {comments &&
+          Object.keys(comments).map((key, index) => {
+            if (typeof comments[key] === "object") {
+              return (
+                <div key={index}>
+                  <p>autor:{comments[key].autor}</p>
+                  <p>terść:{comments[key].comment}</p>
+                </div>
+              );
+            }
+          })}
+        <button onClick={() => setisVisibleAddCommentForm(false)}>
+          Add comment
+        </button>
+        {/* <CommentForm isVisibleForm={isVisibleForm} onCreateComment={onCreateComment} handleCloseForm={handleCloseForm}/> */}
+        <form onSubmit={handleSubmitComment} hidden={isVisibleAddCommentForm}>
           <h5>Add Comment</h5>
-  
-          <textarea value={comment} onChange={handleChangeComment} />
+          <textarea name="contentcom"/>
           <br></br>
           <button type="submit">send comment</button>
-          <button onClick={handleCloseForm}>close form</button>
-        </form> */}
-      </>
-    );
-  }
+          <button onClick={() => setisVisibleAddCommentForm(true)}>
+            close form
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export function CommentForm({
+  isVisibleForm,
+  onCreateComment,
+  handleCloseForm,
+}) {
+  const [comment, setComment] = useState();
+  // const navigate = useNavigate();
+
+  // const handleChangeComment = (e) => {
+  //   setComment(e.target.value);
+  // };
+
+  // const handleSubmitForm = (event) => {
+  //   console.log(comment);
+  //   event.preventDefault();
+  //   onCreateComment({ comment: comment });
+  //   setComment("");
+  //   navigate('/blog');
+
+  // };
+  return <></>;
+}
