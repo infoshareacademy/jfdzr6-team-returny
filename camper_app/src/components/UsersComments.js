cbf28c074427a88387652acfe343c5887bec26b2
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-20 15:00:32 +0200

    add comment ,delete

--- camper_app/src/components/UsersComments.js
+++ camper_app/src/components/UsersComments.js
@@ -1,60 +1,97 @@
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../context/userContext";
 import { getCommentsByCapmerId } from "../api/getCommentsByCamperId";
-import { connectStorageEmulator } from "firebase/storage";
+import { db } from "../firebase";
+import { updateDoc,doc,deleteField  } from "firebase/firestore";
+import uuid from "react-uuid";
+
 export function UsersComments({ camperData }) {
-  const [isVisibleAddCommentForm, setisVisibleAddCommentForm] = useState(true);
+  const [isVisibleAddCommentForm, setisVisibleAddCommentForm] = useState(false);
   const [comments, setComments] = useState();
-  
+
   const context = useContext(UserContext);
-  // if (camperData.campercomments.comments[0]){
-  //     setComments(camperData.campercomments.comments);
-  // }
+
   console.log("id campera:", camperData.id);
   useEffect(() => {
     getCommentsByCapmerId("d3IY7bPvuyt38xZQ5JN3").then((allcomments) => {
-       setComments(allcomments);
-     
+      setComments(allcomments);
     });
   }, []);
 
   console.log(context.userData.name);
   console.log(comments);
-const handleSubmitComment=(e)=>{
+
+  ///////
+  function handleSubmitComment (e) {
     e.preventDefault();
-const newcomment= e.target.contentcom.value;
-console.log(newcomment);
+    const newcomment = e.target.contentcom.value;
+    console.log(newcomment);
+    const docData={
+
+        [uuid()]:{
+
+        comment: newcomment,
+        autor: context.userData.name
+    }
+    }
+    
+    sendEditComment(docData, "d3IY7bPvuyt38xZQ5JN3");
+  };
+
+  function sendEditComment(com, id) {
+    const docRef = doc(db, "comments", id);
+    updateDoc(docRef, com).then(res=>{
+        getCommentsByCapmerId("d3IY7bPvuyt38xZQ5JN3").then((allcomments) => {
+            setComments(allcomments);
+          })
+    }
+
+    );
+  }
+
+  function deleteComment(field,id){
+    const docRef = doc(db, "comments", id);
+    updateDoc(docRef,{
+        [field]: deleteField()
+    }).then(res=>{console.log(res);
+        getCommentsByCapmerId("d3IY7bPvuyt38xZQ5JN3").then((allcomments) => {
+            setComments(allcomments);
+          })
+    
+    })
 }
 
-
+    // deleteComment("1","d3IY7bPvuyt38xZQ5JN3");
 
   return (
     <>
       <div style={{ border: "black 1px solid" }}>
         <h3>Komentarze uzytkowników :</h3>
-        {/* {comments && comments.map((comment,index)=><p key={index}>nr {index+1}: {comment}</p> )} */}
+       
         {comments &&
           Object.keys(comments).map((key, index) => {
             if (typeof comments[key] === "object") {
+                console.log(key)
               return (
                 <div key={index}>
                   <p>autor:{comments[key].autor}</p>
                   <p>terść:{comments[key].comment}</p>
+                  <button onClick={()=>deleteComment(key,"d3IY7bPvuyt38xZQ5JN3")}>Skasuj komentarz</button>
                 </div>
               );
             }
           })}
         <button onClick={() => setisVisibleAddCommentForm(false)}>
-          Add comment
+          Dodaj komentarz
         </button>
-        {/* <CommentForm isVisibleForm={isVisibleForm} onCreateComment={onCreateComment} handleCloseForm={handleCloseForm}/> */}
+       
         <form onSubmit={handleSubmitComment} hidden={isVisibleAddCommentForm}>
-          <h5>Add Comment</h5>
-          <textarea name="contentcom"/>
+          <h5>Napisz Komentarz</h5>
+          <textarea name="contentcom" />
           <br></br>
-          <button type="submit">send comment</button>
+          <button type="submit">Wyślij komentarz</button>
           <button onClick={() => setisVisibleAddCommentForm(true)}>
-            close form
+            Zamknij formularz
           </button>
         </form>
       </div>
@@ -62,25 +99,25 @@ console.log(newcomment);
   );
 }
 
-export function CommentForm({
-  isVisibleForm,
-  onCreateComment,
-  handleCloseForm,
-}) {
-  const [comment, setComment] = useState();
-  // const navigate = useNavigate();
+// export function CommentForm({
+//   isVisibleForm,
+//   onCreateComment,
+//   handleCloseForm,
+// }) {
+//   const [comment, setComment] = useState();
+// const navigate = useNavigate();
 
-  // const handleChangeComment = (e) => {
-  //   setComment(e.target.value);
-  // };
+// const handleChangeComment = (e) => {
+//   setComment(e.target.value);
+// };
 
-  // const handleSubmitForm = (event) => {
-  //   console.log(comment);
-  //   event.preventDefault();
-  //   onCreateComment({ comment: comment });
-  //   setComment("");
-  //   navigate('/blog');
+// const handleSubmitForm = (event) => {
+//   console.log(comment);
+//   event.preventDefault();
+//   onCreateComment({ comment: comment });
+//   setComment("");
+//   navigate('/blog');
 
-  // };
-  return <></>;
-}
+// };
+//   return <></>;
+// }
