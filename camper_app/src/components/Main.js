7dbd2763b43dbe1c2257c4046f7e9fef8daae25d
Marcin Bejger <marcin.bejger@gmail.com>
2022-06-20 10:25:46 +0200

    small functional and visual changes introduced

--- camper_app/src/components/Main.js
+++ camper_app/src/components/Main.js
@@ -1,7 +1,12 @@
-import React from "react";
+import { useContext, React } from "react";
 import './Main.css';
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/userContext";
 
 export const Main = ()=>{
+
+    const context = useContext(UserContext);
+
         return(
 
          <main>
@@ -11,7 +16,26 @@ export const Main = ()=>{
                         <div className="col-lg-12">
                             <h2>WYPOŻYCZ Z NAMI CAMPERA!</h2>
                             <p>Kamper (ang. camper) lub samochód kempingowy to autonomiczny, zintegrowany samochód turystyczny, specjalnie w tym celu zbudowany lub wyposażony, zapewniający podróżującym nim pasażerom miejsca do spania i wypoczynku.</p>
-                            <button> <a href="/add-camper"> Dodaj campera </a> </button>
+                            
+
+
+        {context.userData && (
+          <>
+          {/* <Button> <Link to="/" onClick={()=>{
+            signOut(auth); 
+            context.setUserData('');
+            NotificationManager.info("Zostałeś wylogowany");
+          }}> Wyloguj się</Link></Button> */}
+          <button> <Link to="/add-camper"> Dodaj campera</Link> </button>
+          </>
+        )}
+        {!context.userData && (
+          <button> <Link to="/login"> Dodaj campera</Link></button>
+        )}
+
+
+                            {/* <button> <a href="/add-camper"> Dodaj campera </a> </button> */}
+                        
                         </div>
                     </div>
                 </div>
