ce98ca3cec6812ff51f9b8215b75273bb79b0c3c
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-22 08:06:13 +0200

    finish comments

--- camper_app/src/api/comments/addNewComment.js
+++
@@ -1,7 +0,0 @@
-import { db } from "../../firebase";
-import { updateDoc, doc} from "firebase/firestore";
-
-export function addNewComment(com, id) {
-    const docRef = doc(db, "comments", id);
-   return updateDoc(docRef, com);
-  }
--- camper_app/src/components/UsersComments2.js
+++ camper_app/src/components/UsersComments2.js
@@ -1,55 +1,23 @@
 import { useEffect, useState, useContext } from "react";
-import {
-  collection,
-  doc,
-  addDoc,
-  getDocs,
-  query,
-  serverTimestamp,
-  where,
-  deleteDoc,
-  orderBy,
- 
-} from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
 import { UserContext } from "../context/userContext";
-import { db } from "../firebase";
 import { getCommentsByCamperId } from "../api/comments/getCommentsByCamperId";
+import { addComment } from "../api/comments/addComment";
+import { deleteComment } from "../api/comments/deleteComment";
 
 export function UsersComments2({ camperData }) {
-  const [isVisibleAddCommentForm, setisVisibleAddCommentForm] = useState(true);
   const [comments, setComments] = useState();
 
   const context = useContext(UserContext);
-  console.log(comments);
-  console.log(camperData.id);
 
   useEffect(() => {
     getCommentsByCamperId(camperData.id)
       .then((data) => {
-        console.log(data);
         setComments(data);
       })
       .catch((er) => console.log(er));
   }, []);
 
-  // function getCommentsByCamperId(camperid) {
-  //   const q = query(
-  //     collection(db, "comments2"),
-  //     where("camperId", "==", camperid),
-  //     orderBy("createdAt", "desc")
-  //   );
-  //   return getDocs(q)
-  //     .then((querySnapshot) => {
-  //       return querySnapshot.docs.map((doc) => ({
-  //         id: doc.id,
-  //         ...doc.data(),
-  //       }));
-  //     })
-  //     .then((data) => {
-  //       return data;
-  //     })
-  //     .catch((er) => console.log(er));
-  // }
   function handleSubmitComment(e) {
     e.preventDefault();
     const form = e.target;
@@ -66,23 +34,21 @@ export function UsersComments2({ camperData }) {
       .then((res) => {
         getCommentsByCamperId(camperData.id)
           .then((data) => {
-            console.log(data);
             setComments(data);
           })
           .catch((er) => console.log(er));
       })
       .catch((er) => console.log(er));
-    setisVisibleAddCommentForm(true);
-  }
-
-  function addComment(comment) {
-    const commentsCollection = collection(db, "comments2");
-    return addDoc(commentsCollection, comment);
   }
 
   function handledeleteComment(commentid) {
-    const docRef = doc(db, "comments2", commentid);
-    return deleteDoc(docRef).then;
+    deleteComment(commentid).then((res) => {
+      getCommentsByCamperId(camperData.id)
+        .then((data) => {
+          setComments(data);
+        })
+        .catch((er) => console.log(er));
+    });
   }
 
   return (
@@ -120,20 +86,13 @@ export function UsersComments2({ camperData }) {
             );
           })}
         {context.userData && (
-          <button onClick={() => setisVisibleAddCommentForm(false)}>
-            Dodaj komentarz
-          </button>
+          <form onSubmit={handleSubmitComment}>
+            <h5>Napisz Komentarz</h5>
+            <textarea name="contentcom" />
+            <br></br>
+            <button type="submit">Dodaj komentarz</button>
+          </form>
         )}
-
-        <form onSubmit={handleSubmitComment} hidden={isVisibleAddCommentForm}>
-          <h5>Napisz Komentarz</h5>
-          <textarea name="contentcom" />
-          <br></br>
-          <button type="submit">Wyślij komentarz</button>
-          <button onClick={() => setisVisibleAddCommentForm(true)}>
-            Zamknij formularz
-          </button>
-        </form>
       </div>
     </>
   );
