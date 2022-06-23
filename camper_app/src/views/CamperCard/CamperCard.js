05276d43690ac0a6bd8be2abbaddc1d011bc48f9
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-23 12:21:57 +0200

    add endpoints and refactoring

--- camper_app/src/views/CamperCard/CamperCard.js
+++ camper_app/src/views/CamperCard/CamperCard.js
@@ -4,28 +4,25 @@ import { useState, useEffect } from "react";
 import { getAllCampers } from "../../api/getAllCampers";
 import { getCampersByType } from "../../api/getCampersByType";
 import { getCampersByOnlyRegion } from "../../api/getCampersByOnlyRegion";
+import { getCamperByTypeAndRegion } from "../../api/getCamperByTypeAndRegion";
 import { Loader } from "../../components/Loader";
 import { NotificationManager } from "react-notifications";
 import { Card } from "../../components/Card";
 import { FindCmpr } from "../../components/FindCmpr";
-import { db } from "../../firebase";
-import { getDocs, query, collection, where } from "firebase/firestore";
 
 export const CamperCard = () => {
   const [campers, setCampers] = useState([]);
   const [missCamper, setMissCamper] = useState(false);
   const [search, setSearch] = useState({ type: "", region: "" });
-  console.log(Boolean(search.region));
+
   useEffect(() => {
     if (search.type === "allcapers" && !search.region) {
-      console.log("halo wszystkie");
       getAllCampers().then((data) => {
         setMissCamper(false);
         setCampers(data);
       });
     }
     if (search.type !== "allcapers" && search.region === "") {
-      console.log("pobieram po typie");
       getCampersByType(search.type)
         .then((data) => {
           if (data.length == 0) {
@@ -37,7 +34,6 @@ export const CamperCard = () => {
         .catch((err) => console.log(err));
     }
     if (search.type === "allcapers" && search.region) {
-      console.log("pobieram tylko po regionie");
       getCampersByOnlyRegion(search.region).then((data) => {
         if (data.length == 0) {
           setMissCamper(true);
@@ -47,44 +43,20 @@ export const CamperCard = () => {
       });
     }
     if (search.type !== "allcapers" && search.type !== "" && search.region) {
-      console.log("pobieram po typie i po regionie");
-      getCamperByTypeAndRegion(search.type, search.region);
-    }
-  }, [search]);
-
-  
-
-  
-  
-
-  function getCamperByTypeAndRegion(type, region) {
-    const q = query(
-      collection(db, "campers"),
-      where("campertype", "==", type),
-      where("location", "==", region)
-    );
-    getDocs(q)
-      .then((querySnapshot) => {
-        return querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-      })
-      .then((data) => {
+      getCamperByTypeAndRegion(search.type, search.region).then((data) => {
         if (data.length == 0) {
           setMissCamper(true);
           throw new Error("brak kamperow");
         }
         setCampers(data);
-        console.log(data);
       });
-  }
+    }
+  }, [search]);
+  
 
-  ///////
   useEffect(() => {
     getAllCampers()
       .then((data) => {
-        console.log('campery',data)
         setMissCamper(false);
         setCampers(data);
         return data;
