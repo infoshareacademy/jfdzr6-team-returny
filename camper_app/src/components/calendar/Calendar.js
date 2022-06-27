c01bdd9814702081cf6e23d1378b7532003d3747
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-27 09:46:50 +0200

    creating a loan collection

--- camper_app/src/components/calendar/Calendar.js
+++ camper_app/src/components/calendar/Calendar.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
+import pl from "date-fns/locale/pl";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calendar.style.js";
-// import moment from "moment";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import momentTimezonePlugin from "@fullcalendar/moment-timezone";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import {
   DateInput,
@@ -12,30 +14,27 @@ import {
   StyledWrapper,
   StyledButton,
 } from "./Calendar.style.js";
-import plLocale from '@fullcalendar/core/locales/pl';
-// import momentTimezonePlugin from "@fullcalendar/moment-timezone";
-
+import plLocale from "@fullcalendar/core/locales/pl";
+registerLocale("pl", pl);
 
 const events = [
   {
-    title: "inny camper",
-    allDay: true,
-    start: new Date(2022, 6, 6),
-    end: new Date(2022, 6, 13),
+    title: "zajęty",
+    start: "2022-06-10T09:00:00",
+    end: "2022-06-13T13:00:00",
+  
   },
   {
-    title: "test",
-    start: new Date(2022, 6, 7),
-    end: new Date(2022, 6, 10),
-  },
-  {
-    title: "camper zajety",
-    start: new Date(2022, 6, 20),
-    end: new Date(2022, 6, 23),
+    title: "zajęty",
+    start: "2022-06-14T09:00:00",
+    end: "2022-06-18T01:00:00",
+   
   },
 ];
 
-export function Calendar({camper,user}) {
+export function Calendar({ camper, user }) {
+  console.log(camper)
+  console.log(user)
   const [newEvent, setNewEvent] = useState({
     title: "",
     start: "",
@@ -43,79 +42,87 @@ export function Calendar({camper,user}) {
   });
   const [allEvents, setAllEvents] = useState(events);
 
+
   function handleAddEvent() {
+    const bookingResult = rentalCost();
+    console.log(bookingResult[0],bookingResult[1]);
     setAllEvents([...allEvents, newEvent]);
   }
 
-  let startDate = new Date(newEvent.start)
-  let endDate = new Date(newEvent.end)
-  let dailyRate = camper.price
 
+  console.log(newEvent);
   function rentalCost() {
-      if (startDate != null && endDate != null) {
-        const difference = endDate.getTime() - startDate.getTime()
-        const rentalDuration = Math.ceil(difference / (1000 * 3600 * 24))
-        console.log(rentalDuration)
-        const totalCost = rentalDuration * dailyRate
-        return totalCost
-      }  
+    let dailyRate = camper.price;
+    if (newEvent.start != null && newEvent.end != null) {
+      const difference = newEvent.end - newEvent.start;
+      const rentalDuration = Math.ceil(difference / (1000 * 3600 * 24));
+      const totalCost = rentalDuration * dailyRate;
+      return [totalCost, rentalDuration];
+    }
   }
-  const totalCost = rentalCost()
-  console.log(totalCost)
 
-  console.log(user)
+
   return (
     <div className="Calendar">
       <StyledHeader>Kalendarz wypożyczeń campera</StyledHeader>
       <StyledWrapper>
         <FullCalendar
           locale={plLocale}
-          plugins={[dayGridPlugin]}
-          timeZone="Europe/Warsaw"
+          plugins={[
+            dayGridPlugin,
+            momentTimezonePlugin,
+            resourceTimelinePlugin,
+          ]}
+          timeZone="Europe/Moscow"
+          displayEventTime={false}
           initialView="dayGridMonth"
           events={allEvents}
           contentHeight={450}
         />
       </StyledWrapper>
-        {user && 
-      <CenteredDiv>
-        
+      {user && (
         <CenteredDiv>
-          <DatePicker
-            placeholderText="Data początkowa"
-            selected={newEvent.start}
-            onChange={(start) =>
-              setNewEvent({
-                ...newEvent,
-                start,
-                title: camper.title,
-              })
-            }
-          />
-          <DatePicker
-            placeholderText="Data końcowa"
-            selected={newEvent.end}
-            onChange={(end) =>
-              setNewEvent({
-                ...newEvent,
-                end,
-              })
-            }
-          />
+          <CenteredDiv>
+            <DatePicker
+              locale="pl"
+              placeholderText="Data początkowa"
+              selected={newEvent.start}
+              onChange={(start) => {
+                setNewEvent({
+                  ...newEvent,
+                  start,
+                  title: "zajęty",
+                  camperid:camper.id,
+                  owner:camper.useremail,
+                  ownerid:camper.userid,
+                  borrower:user.email,
+                  borrowerid:user.id
+                });
+              }}
+            />
+            <DatePicker
+              locale="pl"
+              placeholderText="Data końcowa"
+              selected={newEvent.end}
+              onChange={(end) => {
+                setNewEvent({
+                  ...newEvent,
+                  end,
+                });
+              }}
+            />
+          </CenteredDiv>
+
+          <StyledButton
+            style={{
+              margin: "30px",
+            }}
+            onClick={handleAddEvent}
+          >
+            Zarezerwuj campera
+          </StyledButton>
         </CenteredDiv>
-
-        <StyledButton
-          style={{
-            margin: "30px",
-          }}
-          onClick={handleAddEvent}
-        >
-          Zarezerwuj campera
-        </StyledButton>
-
-      </CenteredDiv>
-      
-        }
+      )}
     </div>
   );
 }
