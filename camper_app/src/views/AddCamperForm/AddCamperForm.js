f91408ebb7e3e7e5b915d2962e39790bc1640f45
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-06-29 13:01:58 +0200

    add valid description

--- camper_app/src/views/AddCamperForm/AddCamperForm.js
+++ camper_app/src/views/AddCamperForm/AddCamperForm.js
@@ -102,13 +102,14 @@ export const AddCamperForm = () => {
 
   const validationSchema = yup.object().shape({
     title: yup.string().min(3).max(20).required(),
-    campertype:yup.string().required(),
+    campertype: yup.string().required(),
     year: yup.string().required(),
-    brand:yup.string().required(),
-    capacity:yup.string().required(),
-    price:yup.string().required(),
-    city:yup.string().min(2).required(),
-    location:yup.string().required(),
+    brand: yup.string().required(),
+    capacity: yup.string().required(),
+    price: yup.string().required(),
+    city: yup.string().min(2).required(),
+    location: yup.string().required(),
+    description: yup.string().required(),
   });
 
   return (
@@ -116,7 +117,8 @@ export const AddCamperForm = () => {
       {!sendLoader ? (
         <div>
           <StyledHeader1>Dodaj campera</StyledHeader1>
-          <Formik onSubmit={handleSubmitCamper}
+          <Formik
+            onSubmit={handleSubmitCamper}
             initialValues={{
               title: "",
               campertype: "",
@@ -129,254 +131,209 @@ export const AddCamperForm = () => {
               location: "",
               description: "",
             }}
-            validationSchema={validationSchema}>
+            validationSchema={validationSchema}
+          >
             {({ errors, values, handleChange, handleBlur, isSubmitting }) => (
               <Form>
-              <StyledBoxBackground>
-                <div>
-                  <StyledInputText
-                    name="title"
-                    type="text"
-                    placeholder="Tytuł ogłoszenia"
-                    maxLength="60"
+                <StyledBoxBackground>
+                  <div>
+                    <StyledInputText
+                      name="title"
+                      type="text"
+                      placeholder="Tytuł ogłoszenia"
+                      maxLength="60"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.title}
+                    />
+                    {errors.title ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.title}
+                      </p>
+                    ) : null}
+                  </div>
+
+                  <StyledSelect
+                    name="campertype"
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    value={values.title}
-                  />
-                  {errors.title ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.title}
-                    </p>
-                  ) : null}
-                </div>
-
-                <StyledSelect name="campertype" onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.campertype}>
-                  <option value="">Kategoria campera</option>
-                  <option value="campervan">Campervan</option>
-                  <option value="integra">Integra</option>
-                  <option value="polintegra">Półintegra</option>
-                  <option value="alkowa">Alkowa</option>
-                </StyledSelect>
-                {errors.campertype ? (
+                    value={values.campertype}
+                  >
+                    <option value="">Kategoria campera</option>
+                    <option value="campervan">Campervan</option>
+                    <option value="integra">Integra</option>
+                    <option value="polintegra">Półintegra</option>
+                    <option value="alkowa">Alkowa</option>
+                  </StyledSelect>
+                  {errors.campertype ? (
                     <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
                       {errors.campertype}
                     </p>
                   ) : null}
-                <div>
-                  <StyledInputText
-                    name="year"
-                    type="text"
-                    placeholder="Rocznik"
-                    maxLength="4"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.year}
-                  />
-                  {errors.year ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.year}
-                    </p>
-                  ) : null}
-                </div>
+                  <div>
+                    <StyledInputText
+                      name="year"
+                      type="text"
+                      placeholder="Rocznik"
+                      maxLength="4"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.year}
+                    />
+                    {errors.year ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.year}
+                      </p>
+                    ) : null}
+                  </div>
 
-                <div>
-                  <StyledInputText
-                    name="brand"
-                    type="text"
-                    placeholder="Marka"
-                    maxLength="20"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.brand}
-                  />
-                  {errors.brand ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.brand}
-                    </p>
-                  ) : null}
-                </div>
+                  <div>
+                    <StyledInputText
+                      name="brand"
+                      type="text"
+                      placeholder="Marka"
+                      maxLength="20"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.brand}
+                    />
+                    {errors.brand ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.brand}
+                      </p>
+                    ) : null}
+                  </div>
 
-                <div>
-                  <StyledInputText
-                    name="capacity"
-                    type="text"
-                    placeholder="Ilość osób"
-                    maxLength="1"
+                  <div>
+                    <StyledInputText
+                      name="capacity"
+                      type="text"
+                      placeholder="Ilość osób"
+                      maxLength="1"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.capacity}
+                    />
+                    {errors.capacity ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.capacity}
+                      </p>
+                    ) : null}
+                  </div>
+                  <div>
+                    <StyledInputText
+                      name="price"
+                      type="text"
+                      placeholder="Cena [pln]"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.price}
+                    />
+                    zł/dzień
+                    {errors.price ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.price}
+                      </p>
+                    ) : null}
+                  </div>
+                  <div>
+                    <StyledInputText
+                      name="city"
+                      type="text"
+                      placeholder="Miasto"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.city}
+                    />
+                    {errors.city ? (
+                      <p
+                        style={{ color: "red", margin: "0", fontSize: "12px" }}
+                      >
+                        {errors.city}
+                      </p>
+                    ) : null}
+                  </div>
+                  <StyledSelect
+                    name="location"
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    value={values.capacity}
-                  />
-                  {errors.capacity ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.capacity}
-                    </p>
-                  ) : null}
-                </div>
-                <div>
-                  <StyledInputText
-                    name="price"
-                    type="text"
-                    placeholder="Cena [pln]"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.price}
-                  />
-                  zł/dzień
-                  {errors.price ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.price}
-                    </p>
-                  ) : null}
-                </div>
-                <div>
-                  <StyledInputText
-                    name="city"
-                    type="text"
-                    placeholder="Miasto"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.city}
-                  />
-                  {errors.city ? (
-                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
-                      {errors.city}
-                    </p>
-                  ) : null}
-                </div>
-                <StyledSelect name="location" onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.location}>
-                  <option value="">Lokalizacja campera (województwo)</option>
-                  <option value="mazowieckie">mazowieckie</option>
-                  <option value="slaskie">śląskie</option>
-                  <option value="wielkopolskie">wielkopolskie</option>
-                  <option value="malopolskie">małopolskie</option>
-                  <option value="dolnoslaskie">dolnośląskie</option>
-                  <option value="lodzkie">łódzkie</option>
-                  <option value="pomorskie">pomorskie</option>
-                  <option value="podkarpackie">podkarpackie</option>
-                  <option value="lubelskie">lubelskie</option>
-                  <option value="kujawsko-pomorskie">kujawsko-pomorskie</option>
-                  <option value="zachodniopomorskie">zachodniopomorskie</option>
-                  <option value="warminsko-mazurskie">
-                    warmińsko-mazurskie
-                  </option>
-                  <option value="swietokrzyskie">świętokrzyskie</option>
-                  <option value="podlaskie">podlaskie</option>
-                  <option value="lubuskie">lubuskie</option>
-                  <option value="opolskie">opolskie</option>
-                </StyledSelect>
-                {errors.location ? (
+                    value={values.location}
+                  >
+                    <option value="">Lokalizacja campera (województwo)</option>
+                    <option value="mazowieckie">mazowieckie</option>
+                    <option value="slaskie">śląskie</option>
+                    <option value="wielkopolskie">wielkopolskie</option>
+                    <option value="malopolskie">małopolskie</option>
+                    <option value="dolnoslaskie">dolnośląskie</option>
+                    <option value="lodzkie">łódzkie</option>
+                    <option value="pomorskie">pomorskie</option>
+                    <option value="podkarpackie">podkarpackie</option>
+                    <option value="lubelskie">lubelskie</option>
+                    <option value="kujawsko-pomorskie">
+                      kujawsko-pomorskie
+                    </option>
+                    <option value="zachodniopomorskie">
+                      zachodniopomorskie
+                    </option>
+                    <option value="warminsko-mazurskie">
+                      warmińsko-mazurskie
+                    </option>
+                    <option value="swietokrzyskie">świętokrzyskie</option>
+                    <option value="podlaskie">podlaskie</option>
+                    <option value="lubuskie">lubuskie</option>
+                    <option value="opolskie">opolskie</option>
+                  </StyledSelect>
+                  {errors.location ? (
                     <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
                       {errors.location}
                     </p>
                   ) : null}
-              </StyledBoxBackground>
+                </StyledBoxBackground>
+                <StyledBoxBackground>
+                  <StyledHeader2>Zdjęcia</StyledHeader2>
+                  <DivInfo>Maksymalna ilość zdjęć: 5</DivInfo>
+                  <StyledInputFile
+                    type="file"
+                    onBlur={() => setError("")}
+                    name="imgcollection"
+                    multiple
+                    accept="image/jpg, image/png"
+                  />
+                  <StyledError> {error && error} </StyledError>
+                </StyledBoxBackground>
+                <StyledBoxBackground>
+                  <StyledHeader2>Opis pojazdu:</StyledHeader2>
+                  <div>
+                    <StyledTextArea
+                      name="description"
+                      type="text"
+                      placeholder="Opisz swój pojazd..."
+                      onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.description}
+                    />
+                    {errors.description ? (
+                    <p style={{ color: "red", margin: "0", fontSize: "12px" }}>
+                      {errors.description}
+                    </p>
+                  ) : null}
+                  </div>
+                </StyledBoxBackground>
+                <StyledButton disabled={isSubmitting}>Dodaj campera</StyledButton>
               </Form>
             )}
           </Formik>
-          <form onSubmit={handleSubmitCamper} encType="multipart/form-data">
-            <StyledBoxBackground>
-              <div>
-                <StyledInputText
-                  name="title"
-                  type="text"
-                  placeholder="Tytuł ogłoszenia"
-                  maxLength="60"
-                />
-              </div>
-              <StyledSelect name="campertype">
-                <option value="">Kategoria campera</option>
-                <option value="campervan">Campervan</option>
-                <option value="integra">Integra</option>
-                <option value="polintegra">Półintegra</option>
-                <option value="alkowa">Alkowa</option>
-              </StyledSelect>
-              <div>
-                <StyledInputText
-                  name="year"
-                  type="text"
-                  placeholder="Rocznik"
-                  maxLength="4"
-                />
-              </div>
-              <div>
-                <StyledInputText
-                  name="brand"
-                  type="text"
-                  placeholder="Marka"
-                  maxLength="20"
-                />
-              </div>
-
-              <div>
-                <StyledInputText
-                  name="capacity"
-                  type="text"
-                  placeholder="Ilość osób"
-                  maxLength="1"
-                />
-              </div>
-              <div>
-                <StyledInputText
-                  name="price"
-                  type="text"
-                  placeholder="Cena [pln]"
-                />{" "}
-                zł/dzień
-              </div>
-              <div>
-                <StyledInputText name="city" type="text" placeholder="Miasto" />
-              </div>
-              <StyledSelect name="location">
-                <option value="">Lokalizacja campera (województwo)</option>
-                <option value="mazowieckie">mazowieckie</option>
-                <option value="slaskie">śląskie</option>
-                <option value="wielkopolskie">wielkopolskie</option>
-                <option value="malopolskie">małopolskie</option>
-                <option value="dolnoslaskie">dolnośląskie</option>
-                <option value="lodzkie">łódzkie</option>
-                <option value="pomorskie">pomorskie</option>
-                <option value="podkarpackie">podkarpackie</option>
-                <option value="lubelskie">lubelskie</option>
-                <option value="kujawsko-pomorskie">kujawsko-pomorskie</option>
-                <option value="zachodniopomorskie">zachodniopomorskie</option>
-                <option value="warminsko-mazurskie">warmińsko-mazurskie</option>
-                <option value="swietokrzyskie">świętokrzyskie</option>
-                <option value="podlaskie">podlaskie</option>
-                <option value="lubuskie">lubuskie</option>
-                <option value="opolskie">opolskie</option>
-              </StyledSelect>
-            </StyledBoxBackground>
-
-            <StyledBoxBackground>
-              <StyledHeader2>Zdjęcia</StyledHeader2>
-              <DivInfo>Maksymalna ilość zdjęć: 5</DivInfo>
-              <StyledInputFile
-                type="file"
-                onBlur={() => setError("")}
-                name="imgcollection"
-                multiple
-                accept="image/jpg, image/png"
-              />
-              <StyledError> {error && error} </StyledError>
-            </StyledBoxBackground>
-
-            <StyledBoxBackground>
-              <StyledHeader2>Opis pojazdu:</StyledHeader2>
-              <div>
-                <StyledTextArea
-                  name="description"
-                  type="text"
-                  placeholder="Opisz swój pojazd..."
-                />
-              </div>
-            </StyledBoxBackground>
-
-            <StyledButton>Dodaj campera</StyledButton>
-          </form>
+          
         </div>
       ) : (
         <Loader />
