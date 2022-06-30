3079c4570bf819303bafe9a7bdab26635fa35424
Marcin Bejger <marcin.bejger@gmail.com>
2022-06-30 18:09:57 +0200

    user panel started

---
+++ camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { getAllCampers } from "../../api/getAllCampers";
+import { getCampersByType } from "../../api/getCampersByType";
+import { Loader } from "../../components/Loader";
+import { NotificationManager } from "react-notifications";
+import { Card } from "../../components/Card";
+import { FindCmpr } from "../../components/FindCmpr";
+
+import { StyledHeader1 } from "./CamperOwnerPanel.style";
+
+export const CamperOwnerPanel = () => {
+
+    // const UserAllCampers = 
+
+    const [myCampers, setMyCampers] = useState([]);
+    const [missCamper, setMissCamper] = useState(false);
+    const [search, setSearch] = useState({ type: "", region: "" });
+  
+    useEffect(() => {
+      if (search.type === "allcapers" && !search.region) {
+        getAllCampers().then((data) => {
+          setMissCamper(false);
+          setMyCampers(data);
+        });
+      }
+      if (search.type !== "allcapers" && search.region === "") {
+        getCampersByType(search.type)
+          .then((data) => {
+            if (data.length == 0) {
+              setMissCamper(true);
+              throw new Error("Nie masz żadnych camperów");
+            }
+            setMyCampers(data);
+          })
+          .catch((err) => console.log(err));
+      }
+    }, [search]);
+    
+  
+    useEffect(() => {
+      getAllCampers()
+        .then((data) => {
+          setMissCamper(false);
+          setMyCampers(data);
+          return data;
+        })
+        .then((err) => {
+          if (err.length == 0) {
+            NotificationManager.error("coś poszło nie tak");
+          }
+        });
+    }, []);
+
+    return (
+      <>
+      <StyledHeader1>Lista moich camperów</StyledHeader1>
+        {myCampers.length !== 0 ? (
+          <>
+            {/* <FindCmpr  setSearch={setSearch} setMissCamper={setMissCamper} /> */}
+            {!missCamper ? (
+              <div className="wrapper">
+                {myCampers && myCampers.map((el) => <Card key={el.id} data={el} />)}
+              </div>
+            ) : (
+              <h3 style={{ textAlign: "center", marginBottom: "50px" }}>
+                Brak kamperów do wyświetlenia
+              </h3>
+            )}
+          </>
+        ) : (
+          <Loader />
+        )}
+      </>
+    );
+};
---
+++ camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledHeader1 = styled.h1`
+  font-family: 'Play', sans-serif;
+  margin: 20px 0px 20px 50px;
+  color: #519a97;
+  font-size: 50px;
+`;
