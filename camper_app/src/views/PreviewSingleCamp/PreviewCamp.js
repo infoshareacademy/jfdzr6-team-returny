d246eca61084672171e087ece6116a780c5ad5b7
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-07-01 14:21:08 +0200

    update camper

--- camper_app/src/views/PreviewSingleCamp/PreviewCamp.js
+++ camper_app/src/views/PreviewSingleCamp/PreviewCamp.js
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { useParams,useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/userContext";
 import {
   StyledDescriptionBox,
@@ -21,6 +21,7 @@ import { NotificationManager } from "react-notifications";
 import "react-notifications/lib/notifications.css";
 import { getCamperById } from "../../api/geCamperById";
 import { deleteCamper } from "../../api/deleteCamper";
+import { updateCamper } from "../../api/updateCamper";
 import { UsersComments2 } from "../../components/UsersComments2";
 import { Calendar } from "../../components/calendar/Calendar";
 import MyGallery from "../../components/MyGallery";
@@ -28,9 +29,9 @@ import MyGallery from "../../components/MyGallery";
 export function PreviewCamp() {
   const [camper, setCamper] = useState();
   const [isEdit, setisEdit] = useState(false);
-  const [newPrice,setNewPrice]= useState('');
-  const [newDescription,setnewDescription]= useState('');
-  
+  const [newPrice, setNewPrice] = useState("");
+  const [newDescription, setnewDescription] = useState("");
+
   const params = useParams();
   const { id } = params;
   const navigate = useNavigate();
@@ -44,86 +45,100 @@ export function PreviewCamp() {
       .catch((er) => console.log(er));
   }, []);
 
-function deleteCamperHandler(id){
-  deleteCamper(id).then(res=>{
-    NotificationManager.success("Kamper został usunięty");
-    navigate('/find-camper')
-  }).catch(er=>{
-    NotificationManager.error("Coś poszło nie tak");
-    console.log(er)})
-}
-console.log(context.userData.id);
-console.log(camper)
+  function deleteCamperHandler(id) {
+    deleteCamper(id)
+      .then((res) => {
+        NotificationManager.success("Kamper został usunięty");
+        navigate("/find-camper");
+      })
+      .catch((er) => {
+        NotificationManager.error("Coś poszło nie tak");
+        console.log(er);
+      });
+  }
+  console.log(context.userData.id);
+  console.log(camper);
 
-function handleChangePrice(e) {
-  setNewPrice(e.target.value);
-  console.log(newPrice)
-}
+  function handleChangePrice(e) {
+    setNewPrice(e.target.value);
+    console.log(newPrice);
+  }
 
-function handleChangeDescription(e) {
-  setnewDescription(e.target.value);
-  console.log(newDescription)
-}
-function handleSubmitChange(e){
-  e.preventDefault();
-  console.log(newPrice)
-  console.log(newDescription)
-}
+  function handleChangeDescription(e) {
+    setnewDescription(e.target.value);
+    console.log(newDescription);
+  }
+  function handleSubmitChange(e) {
+    e.preventDefault();
+    updateCamper(id, newPrice, newDescription)
+      .then((res) => {
+        getCamperById(id)
+          .then((data) => {
+            setCamper(data);
+          })
+          .catch((er) => console.log(er));
+        NotificationManager.success("Kamper został zaktualizowany");
+        setisEdit(false);
+      })
+      .catch((er) => NotificationManager.error("Coś poszło nie tak"));
+  }
   return (
     <>
       {camper && (
         <Wrapper>
-
           <CampTitle>
             <h2>{camper.title}</h2>
           </CampTitle>
 
           <MyGallery camper={camper} />
-        {context.userData.id===camper.userid &&
-          <ButtonsSection>
-            <StyledEditButton onClick={()=>setisEdit(true)}>
-              Edytuj ogłoszenie
-            </StyledEditButton>
-            <StyledEditButton onClick={()=>deleteCamperHandler(id)}>
-              Usuń ogłoszenie
-            </StyledEditButton>
-          </ButtonsSection>
-        
-        }
-          {isEdit ? 
-          <StyledCampDetails>
-      <form onSubmit={handleSubmitChange}>
-        cena
-        <input
-          value={newPrice}
-          onChange={handleChangePrice}
-          placeholder="Zmień cene"
-          required
-        />
-        opis
-        <textarea
-          value={newDescription}
-          onChange={handleChangeDescription}
-          placeholder="Zmień opis"
-          required
-        />
-        <button type="submit">Zatwierdź zmiany</button>
-        <button onClick={(e)=>{
-          e.preventDefault();
-          setisEdit(false)}
-
-        }>
-            Zrezygnuj</button>
-      </form>
-
-          </StyledCampDetails> : null
-          }
-
+          {context.userData.id === camper.userid && (
+            <ButtonsSection>
+              <StyledEditButton onClick={() => setisEdit(true)}>
+                Edytuj ogłoszenie
+              </StyledEditButton>
+              <StyledEditButton onClick={() => deleteCamperHandler(id)}>
+                Usuń ogłoszenie
+              </StyledEditButton>
+            </ButtonsSection>
+          )}
+          {isEdit ? (
+            <StyledCampDetails>
+              <form onSubmit={handleSubmitChange}>
+                <div>
+                  cena
+                  <input
+                    value={newPrice}
+                    onChange={handleChangePrice}
+                    placeholder="Zmień cene"
+                    required
+                  />
+                </div>
+                <div>
+                  opis
+                  <textarea
+                    value={newDescription}
+                    onChange={handleChangeDescription}
+                    placeholder="Zmień opis"
+                    required
+                  />
+                </div>
+                <button type="submit">Zatwierdź zmiany</button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setisEdit(false);
+                  }}
+                >
+                  Zrezygnuj
+                </button>
+              </form>
+            </StyledCampDetails>
+          ) : null}
 
           <StyledCampDetails>
             <h2>O camperze:</h2>
             <p>Kategoria: {camper.campertype}</p>
-            <p>Rocznik : {camper.year}</p> 
+            <p>Rocznik : {camper.year}</p>
             <p>Marka : {camper.brand}</p>
             <p>Ilość osób : {camper.papacity}</p>
             <p>Cena (zł/dzień) : {camper.price}</p>
