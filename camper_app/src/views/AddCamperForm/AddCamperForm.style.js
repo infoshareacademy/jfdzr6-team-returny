ed5884a5f2f58c6ab6a21f1a4087b760bcd1116a
Daria Kolesnik <dariakol91@gmail.com>
2022-07-01 23:29:55 +0200

    form changed

--- camper_app/src/views/AddCamperForm/AddCamperForm.style.js
+++ camper_app/src/views/AddCamperForm/AddCamperForm.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledHeader1 = styled.h1`
   font-family: 'Play', sans-serif;
-  margin: 20px 0px 20px 50px;
+  margin: 104px 0px 40px 50px;
   color: #519a97;
   font-size: 50px;
 `;
