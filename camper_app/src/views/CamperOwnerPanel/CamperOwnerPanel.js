0314d223176cdf91b840f041599bc91e8fbb0497
Marcin Bejger <marcin.bejger@gmail.com>
2022-07-02 17:53:52 +0200

    user panel updated

--- camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.js
+++ camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.js
@@ -4,25 +4,28 @@ import { Loader } from "../../components/Loader";
 import { CardOwner } from "./CardOwner";
 import { StyledHeader1, StyledWrapper } from "./CamperOwnerPanel.style";
 import { UserContext } from "../../context/userContext";
-
 export const CamperOwnerPanel = () => {
   const { userData } = useContext(UserContext);
   const [myCampers, setMyCampers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const fetchCampers = async () => {
+    if (!userData.id) return;
     try {
       const campers = await getCampersByUserId(userData.id);
       setMyCampers(campers);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
     }
   };
   useEffect(() => {
     fetchCampers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userData]);
   return (
     <>
       <StyledHeader1>Lista moich camperów</StyledHeader1>
-      {myCampers.length ? (
+      {!isLoading ? (
         <StyledWrapper>
           {myCampers.map((el) => (
             <CardOwner key={el.id} data={el} />
