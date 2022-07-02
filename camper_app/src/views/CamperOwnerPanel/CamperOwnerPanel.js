2196c86524ffae2ab4403ce573d4757285fa937e
Marcin Bejger <marcin.bejger@gmail.com>
2022-07-02 17:13:44 +0200

    user panel edited

--- camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.js
+++ camper_app/src/views/CamperOwnerPanel/CamperOwnerPanel.js
@@ -1,77 +1,32 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { getAllCampers } from "../../api/getAllCampers";
+import { useState, useEffect, useContext } from "react";
 import { getCampersByUserId } from "../../api/getCampersByUserId";
 import { Loader } from "../../components/Loader";
-import { NotificationManager } from "react-notifications";
 import { CardOwner } from "./CardOwner";
-import { StyledHeader1, StyledWrapper } from "./CamperOwnerPanel.style"
-
-// sdk... do pobrania camperow
-// getDocs, filter po userid uzytkownika (ciag znakow)
-// useEffect
-// 
-// 
+import { StyledHeader1, StyledWrapper } from "./CamperOwnerPanel.style";
+import { UserContext } from "../../context/userContext";
 
 export const CamperOwnerPanel = () => {
+  const context = useContext(UserContext);
+  const [myCampers, setMyCampers] = useState([]);
 
-    const [myCampers, setMyCampers] = useState([]);
-    const [missCamper, setMissCamper] = useState(false);
-    const [search, setSearch] = useState({ userid: "W5wsue1D3GZWrO8Nf8cqwFVtIfx2" });
-  
-    useEffect(() => {
-      // if (search.type === "allcapers" && !search.region) {
-      //   getAllCampers().then((data) => {
-      //     setMissCamper(false);
-      //     setMyCampers(data);
-      //   });
-      // }
-      if (search.type !== "allcapers" && search.userid === "W5wsue1D3GZWrO8Nf8cqwFVtIfx2") {
-        getCampersByUserId(search.userid)
-          .then((data) => {
-            if (data.length == 0) {
-              setMissCamper(true);
-              throw new Error("Nie masz żadnych camperów");
-            }
-            setMyCampers(data);
-          })
-          .catch((err) => console.log(err));
-      }
-    }, [search]);
-    
-    useEffect(() => {
-      getAllCampers()
-        .then((data) => {
-          setMissCamper(false);
-          setMyCampers(data);
-          return data;
-        })
-        .then((err) => {
-          if (err.length == 0) {
-            NotificationManager.error("coś poszło nie tak");
-          }
-        });
-    }, []);
+  useEffect(() => {
+    console.log(context);
+    getCampersByUserId(context.userData.id)
+      .then((data) => {
+        setMyCampers(data);
+      })
+      .catch((err) => console.log(err));
+  }, [context.userData.id]);
 
-    return (
-      <>
-        <StyledHeader1>Lista moich camperów</StyledHeader1>
-        {myCampers.length !== 0 ? (
-          <>
-            {/* <FindCmpr  setSearch={setSearch} setMissCamper={setMissCamper} /> */}
-            {!missCamper ? (
-              <StyledWrapper>
-                {myCampers && myCampers.map((el) => <CardOwner key={el.id} data={el} />)}
-              </StyledWrapper>
-            ) : (
-              <h3 style={{ textAlign: "center", marginBottom: "50px" }}>
-                Brak kamperów do wyświetlenia
-              </h3>
-            )}
-          </>
-        ) : (
-          <Loader />
-        )}
-      </>
-    );
+  return (
+    <>
+      <StyledHeader1>Lista moich camperów</StyledHeader1>
+        <StyledWrapper>
+          {myCampers &&
+            context.userData &&
+            myCampers.map((el) => <CardOwner key={el.id} data={el} />)}
+        </StyledWrapper>
+    </>
+  );
 };
